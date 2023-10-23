be384d8e8ee4073c1c2a3dd909e52cbc98c13add
lastsamurai24 <lastsamurai24@icloud.com>
2023-10-23 12:18:00 +0900

    calcs

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    public function calcs($number1, $operator, $number2,)
+    {
+        $result = 0;
+        switch ($operator) {
+            case 'addition':
+                $result = $number1 + $number2;
+                break;
+            case 'subtraction':
+                $result = $number1 - $number2;
+                break;
+            case 'multiplication':
+                $result = $number1 * $number2;
+                break;
+            case 'division':
+                if ($number2 != 0) {
+                    $result = $number1 / $number2;
+                } else {
+                    $result = 'Division by zero error';
+                }
+                break;
+            default:
+                $result = 'Invalid operator';
+        }
+        return view('message.calcs', [
+            'number1' => $number1,
+            'operator' => $operator,
+            'number2' => $number2,
+            'result' => $result
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/view/calcs/{number1}/{operator}/{number2}', function ($number1, $operator, $number2,) {
+    $result = 0;
+    switch ($operator) {
+        case 'addition':
+            $result = $number1 + $number2;
+            break;
+        case 'subtraction':
+            $result = $number1 - $number2;
+            break;
+        case 'multiplication':
+            $result = $number1 * $number2;
+            break;
+        case 'division':
+            if ($number2 != 0) {
+                $result = $number1 / $number2;
+            } else {
+                $result = 'Division by zero error';
+            }
+            break;
+        default:
+            $result = 'Invalid operator';
+    }
+
+    return view('message.calcs', [
+        'number1' => $number1,
+        'operator' => $operator,
+        'number2' => $number2,
+        'result' => $result
+    ]);
+});
+Route::get('/calcs/{number1}/{operator}/{number2}', [MessageController::class, 'calcs']);
